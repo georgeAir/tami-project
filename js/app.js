ec49f2bc6eb6055c785c1b4f7441260b85f742fe
george-arias <georgearias08@gmail.com>
2021-12-06 12:41:24 -0600

    added buttons and background

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log('tamagachi');
 
 
-
+////////////////////////////// OBJECT ////////////////////////////////////////
 class Tamagachi {
   constructor (name) {
     this.hunger= 4
@@ -9,7 +9,16 @@ class Tamagachi {
     this.boredom= 1
     this.age=1
   }
+  
+
 }
 
 const pet = new Tamagachi('steve')
 console.log(pet);
+
+
+////////////////////////////////// BUTTONS ///////////////////////////////////
+
+const aButton = document.querySelector('.food')
+const bButton = document.querySelector('.lights')
+const cButton = document.querySelector('.play')
