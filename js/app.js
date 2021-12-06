ac8ba7b26f35ccac1fdb71203c60a9332f05e67d
george-arias <georgearias08@gmail.com>
2021-12-06 11:10:19 -0600

    instantiate pet

--- js/app.js
+++ js/app.js
@@ -2,8 +2,14 @@ console.log('tamagachi');
 
 
 
-class ANIMAL {
-  constructor () {
-    
+class Tamagachi {
+  constructor (name) {
+    this.hunger= 4
+    this.sleepiness= 5
+    this.boredom= 1
+    this.age=1
   }
 }
+
+const pet = new Tamagachi('steve')
+console.log(pet);
